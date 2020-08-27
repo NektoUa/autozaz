ac824a85e9d58ab3412507a2f7a1770a4ce8d0ea
NektoUa <illyabarkov@gmail.com>
2020-08-27 06:42:36 +0300

    add slick-slider

---
+++ js/scriptSlick.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('.slider').slick();
+});
+// $('.slider').slick({
+//     arrows: true,
+//     dots: true,
+//     adaptiveHeight: true,
+//     slidesToShow: 2,
+//     speed: 500,
+//     responsive: [
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 1,
+//             }
+//         }
+//     ]
+// });
+// $('.slider').on('afterChange', function (event, slick, currentSlide) {
+//     console.log(currentSlide);
+// })
