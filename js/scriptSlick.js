b457583b1d5f0798549d3eaaec574d27af4478d8
NektoUa <illyabarkov@gmail.com>
2020-08-31 20:20:43 +0300

    repair

--- js/scriptSlick.js
+++ js/scriptSlick.js
@@ -1,22 +1,27 @@
 $(document).ready(function () {
     $('.slider').slick({
         infinite: true,
+        centerMode: true,
+        arrows: false,
         dots: true,
         autoplay: true,
         autoplaySpeed: 4000,
         adaptiveHeight: true,
         fade: true,
         pauseOnHover: true,
+
     });
     $('.slider-news').slick({
         infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
         dots: false,
         autoplay: true,
         autoplaySpeed: 5000,
         adaptiveHeight: true,
         pauseOnHover: true,
+        centerMode: true,
         responsive: [
             {
                 breakpoint: 480,
